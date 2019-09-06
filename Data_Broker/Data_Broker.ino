c8c8f08c2e5370873e76f7b93e16e594cf80b2c2
llamington <54869395+llamington@users.noreply.github.com>
2019-09-06 22:21:58 +1200

    Saved values into a String Array

--- Data_Broker/Data_Broker.ino
+++ Data_Broker/Data_Broker.ino
@@ -8,6 +8,7 @@ const int loRaM1 = 9;
 const int rainPin = 7;
 int tips = 0;
 String loRaBuffer;
+String values[8];
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,9 +29,10 @@ void loop() {
     delay(500);
     Serial.println(tips);
   }
-  while(loRa.available() > 0) {
-    loRaBuffer = loRa.readStringUntil('>');
-    Serial.println(loRaBuffer);
+  for(int i = 0; loRa.available() > 0; i++) { // runs while serial buffer is occupied
+    values[i] = loRa.readStringUntil('>');
+    Serial.println(values[i]);
+    
   }
    delay(1000);
   
