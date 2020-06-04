6ebce304a90b6630cac637f7b3dc73bb963da84d
llamington <54869395+llamington@users.noreply.github.com>
2020-06-04 12:07:08 +1200

    Moved global vars to local

--- Data_Broker/Data_Broker.ino
+++ Data_Broker/Data_Broker.ino
@@ -16,25 +16,15 @@ byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 EthernetClient client;
 
 Adafruit_MQTT_Client mqtt(&client, "io.adafruit.com", 1883, mqttUsername, "4a179569d20d440988bc9cb7d1a75a8f");
-Adafruit_MQTT_Publish publishArray[] = {Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/air-humidity"),
-Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/air-temperature"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/ultraviolet-a"),
-Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/ultraviolet-b"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/ultraviolet-index"),
-Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/soil-humidity"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/soil-temperature"),
-Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/soil-ph"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/battery-percentage")};
+
 #define halt(s) { Serial.println(F( s )); while(1);  }
 
 
 const byte loRaM0 = 12;
 const byte loRaM1 = 9;
 const byte rainPin = 7;
-const int chipSelect = 4;
+const byte chipSelect = 4;
 unsigned int tips = 0;
-const char startDelimiter[] = {'H', 'T', 'A', 'B', 'I', 'M', 'S', 'P', 'C'};
-const String address[] = {"air-humidity", "air-temperature", "ultraviolet-a", "ultraviolet-b", "ultraviolet-index", "soil-humidity", "soil-temperature", "soil-ph", "battery-percentage"};
-float receivedArray[9];
-
-String received;
-byte startString;
 
 
 void setup() {
@@ -45,13 +35,27 @@ void setup() {
   Serial.begin(9600);
   loRa.begin(9600);
   Ethernet.begin(mac);
-  SD.begin(chipSelect);
+  if (!SD.begin(chipSelect)) {
+    Serial.println(F("Card failed, or not present"));
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println(F("card initialized."));
   delay(2000);
   Serial.print(F("Initialised"));
   
 }
 
 void loop() {
+  Adafruit_MQTT_Publish publishArray[] = {Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/air-humidity"),
+  Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/air-temperature"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/ultraviolet-a"),
+  Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/ultraviolet-b"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/ultraviolet-index"),
+  Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/soil-humidity"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/soil-temperature"),
+  Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/soil-ph"), Adafruit_MQTT_Publish(&mqtt, mqttUsername "/feeds/battery-percentage")};
+  String received;
+  byte startString;
+  const char startDelimiter[] = {'H', 'T', 'A', 'B', 'I', 'M', 'S', 'P', 'C'};
+  float receivedArray[9];
   // put your main code here, to run repeatedly:
   MQTT_connect();
   digitalWrite(loRaM0, LOW);
@@ -65,25 +69,22 @@ void loop() {
   if(loRa.available() > 0) {
     delay(500);
     if(dataFile){
-      dataFile.print("Bytes in buffer: ");
       dataFile.println(loRa.available());
-      dataFile.print("Free memory: ");
       dataFile.println(freeMemory());
-      for(int i = 0; i <= 8; i++) {
-          dataFile.println(address[i] + ": " + receivedArray[i]);
-      }
     }
-    Serial.println(loRa.available());
     received = loRa.readString();
     Serial.println(received);
+    if(dataFile){
+      dataFile.println(received);
+    }
     for(int i = 0; i <= 8; i++) {
       startString = received.indexOf(startDelimiter[i]); // finds index of starting delimiter
       receivedArray[i] = received.substring(startString+1, received.indexOf('>', startString)).toFloat();
-      Serial.println(address[i] + ": " + receivedArray[i]);
+      if(dataFile){
+        dataFile.println(String(startDelimiter[i]) + ": " + receivedArray[i]);
+      }
       publishArray[i].publish(receivedArray[i]);
     }
-    Serial.print(F("Free Memory: "));
-    Serial.println(freeMemory());
   }
   delay(100);
 }
